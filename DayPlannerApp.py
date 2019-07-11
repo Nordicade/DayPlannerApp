1e9f3f0a630c51ebce2bf24ef01d2e5723e02e9b
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-07-10 18:07:20 -0600

    Struggling to fit bottom button
    Add activity button has left the layout area

--- DayPlannerApp.py
+++ DayPlannerApp.py
@@ -12,32 +12,6 @@ from kivy.graphics import Color, Rectangle
 from kivy.config import Config
 from kivy.core.window import Window
 
-class MyGrid(GridLayout) :
-    def __init__(self, **keywordArgs):
-        super(MyGrid, self).__init__(**keywordArgs)
-        self.cols = 2
-        self.rows = 2
-        self.add_widget(Label(text="Name: "))
-        self.add_widget(Label(text="Name: "))
-        self.add_widget(Label(text="Name: "))
-        self.add_widget(Label(text="Name: "))
-
-class MyStack(StackLayout) :
-    def __init__(self, **keywordArgs):
-        super(MyStack, self).__init__(**keywordArgs)
-        self.cols = 2
-        self.rows = 2
-        self.add_widget(Label(text="Name: "))
-        self.add_widget(Label(text="Age: "))
-
-class MyBox(BoxLayout) :
-    def __init__(self, **keywordArgs):
-        super(MyBox, self).__init__(**keywordArgs)
-        self.cols = 2
-        self.rows = 2
-        self.add_widget(Label(text="Name: "))
-        self.add_widget(Label(text="Age: "))
-
 class UnderlyingGridLayout(GridLayout):
     pass
 
