cee5ae3e0cfb6996eccfd312e3b1c414707a5d58
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-07-12 13:01:14 -0600

    Underlying layout is now holding inner 3 layouts!
    Now to add scrollView to middle_layout, set size of bottom_layout, and figure out how I want to display the top_layout

--- DayPlanner1App.py
+++ DayPlanner1App.py
@@ -7,6 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.graphics import Color, Rectangle
 from kivy.uix.widget import Widget
@@ -14,16 +15,28 @@ from kivy.uix.widget import Widget
 class CustomWidgets(Widget):
     pass
 
+class CustomGridLayout(GridLayout):
+    pass
+
+class CustomStackLayout(StackLayout):
+    pass
+
+class CustomBoxLayout(BoxLayout):
+    pass
+
 class CustomAnchorLayout(AnchorLayout):
     pass
 
 class DayPlanner1App(App):
     def build(self):
-        #underlying_layout = GridLayout(orientation = 'vertical')
-        #top_layout =
+        underlying_layout = CustomGridLayout()
+        #top_layout = CustomStackLayout()
         #mid_layout = CustomBoxLayout()
-        bottom_layout = CustomAnchorLayout()
-        return CustomAnchorLayout()
+        #bottom_layout = CustomAnchorLayout()
+        #underlying_layout.add(top_layout)
+        #underlying_layout.add(mid_layout)
+        #underlying_layout.add(bottom_layout)
+        return underlying_layout
 
 if __name__ == "__main__":
     DayPlanner1App().run()
