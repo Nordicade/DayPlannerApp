867a0fbdcd1aa299bd903ba11c04cde5c52be3b5
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-07-13 21:53:13 -0600

    Added global variable to track widget #

--- DayPlanner1App.py
+++ DayPlanner1App.py
@@ -13,7 +13,7 @@ from kivy.graphics import Color, Rectangle
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 
-increment = 1
+widget_count = 0
 
 class CustomWidgets(StackLayout):
     def bump_activity(self):
@@ -28,6 +28,16 @@ class CustomWidgets(StackLayout):
 
 class CustomGridLayout(GridLayout):
     def bump_activity(self):
+        # keep in mind, self is the CustomWidget
+        global widget_count
+        widget_count = widget_count - 1
+
+        # set the height of ScrollView/GridLayout to be (widget_count * 100) + label_height
+        grid_layout = self.parent
+        grid_layout.height = (widget_count * 120) + 20  
+        #list = self.ids['activity_list_layout']
+        #scroll = list.ids['scroller']
+
         self.remove_widget(self.children[0])
         self.remove_widget(self.children[0])
         self.remove_widget(self.children[0])
@@ -35,10 +45,11 @@ class CustomGridLayout(GridLayout):
         print("bump_activity")
 
     def add_activity(self):
-        print("add_activity")
-        # keep in mind, self is the CustomGridLayout 
+        # keep in mind, self is the CustomGridLayout
+        global widget_count
+        widget_count = widget_count + 1
         self.ids['activity_list_layout'].add_widget(CustomWidgets())
-        #https://stackoverflow.com/questions/46984515/kivy-adding-buttons-in-a-sub-layout-when-a-button-is-pressed-on-release
+        print("add_activity")
 
     def modify_activity(self):
         print("modify_activity")
