65cdef05ac9ad824c177c05b2c990feaebb2b2fa
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-07-15 14:56:55 -0600

    Switched back to DayPlannerApp.py and .kv
    Also included time bar and new CustomLabels class

--- DayPlanner1App.py
+++ DayPlanner1App.py
@@ -7,8 +7,8 @@ from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
@@ -17,22 +17,26 @@ widget_count = 0
 
 class CustomWidgets(StackLayout):
     def bump_activity(self):
-        print(self)
+        print(self.parent)
+
         grid_layout = self.parent
         grid_layout.remove_widget(self)
+        print("bump_activity")
+
+
+    def remove_activity(self):
+        grid_layout = self.parent
+        grid_layout.remove_widget(self)
+        print("remove_activity")
+
     pass
 
 class CustomGridLayout(GridLayout):
     def add_activity(self):
-        # keep in mind, self is the CustomGridLayout
         global widget_count
         widget_count = widget_count + 1
         self.ids['activity_list_layout'].add_widget(CustomWidgets())
         print("add_activity")
-
-    def modify_activity(self):
-        print("modify_activity")
-
     pass
 
 class DayPlanner1App(App):
--- DayPlannerApp.py
+++ DayPlannerApp.py
@@ -4,24 +4,59 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
-from kivy.config import Config
-from kivy.core.window import Window
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
 
-class UnderlyingGridLayout(GridLayout):
+widget_count = 0
+
+class CustomWidgets(StackLayout):
+    def bump_activity(self):
+        CustomGridLayout.display_activity(self)
+        grid_layout = self.parent
+        grid_layout.remove_widget(self)
+        print("bump_activity")
+
+
+    def remove_activity(self):
+        grid_layout = self.parent
+        grid_layout.remove_widget(self)
+        print("remove_activity")
+
+    pass
+
+class CustomLabels(Label):
+    #retrieve color based upon widget widget_count
+    #check pos and size of existing CustomWidgets
+    #place new label at first remaining spot
+    pass
+
+class CustomGridLayout(GridLayout):
+    def add_activity(self):
+        global widget_count
+        widget_count = widget_count + 1
+        self.ids['activity_list_layout'].add_widget(CustomWidgets())
+        print("add_activity")
+
+    def display_activity(self):
+        activity_text = self.ids['partB'].text
+        underlying_layout = self.parent.parent.parent
+        print(underlying_layout)
+        #top_layout = underlying_layout.ids['top_layout']
+        #top_layout.ids['am_layout'].add_widget(CustomLabels())
+        underlying_layout.ids['am_layout'].add_widget(CustomLabels())
+        print("grid_layout says: " + activity_text)
     pass
 
 class DayPlannerApp(App):
     def build(self):
-        return UnderlyingGridLayout()
-
-Config.set('graphics','resizable',0)
-Config.write()
-Window.size = (600, 800)
+        underlying_layout = CustomGridLayout()
+        return underlying_layout
 
 
 if __name__ == "__main__":
