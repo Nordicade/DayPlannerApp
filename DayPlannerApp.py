8a6b6bb9c22a008d71b64508b6a09603079c6cef
Nordicade <46762778+Nordicade@users.noreply.github.com>
2019-07-17 19:41:28 -0600

    Implemented my own damn drag method.
    Only bug now is it doesn't know when to let go and select a different CustomLabels object.

--- DayPlannerApp.py
+++ DayPlannerApp.py
@@ -8,7 +8,6 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scatterlayout import ScatterLayout
-from kivy.uix.scatter import Scatter
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
@@ -19,6 +18,7 @@ from kivy.uix.behaviors import DragBehavior
 #if you ctrl+z to this point, you've gone too far
 
 widget_count = 0
+mouse_down = False
 color_array = [[128/255, 0 , 0, 0], [0, 128/255, 0, 0], [0, 128/255, 128/255, 0], [1, 1, 0, 0],
  [128/255, 0, 128/255, 0], [1, 0, 0, 0], [0, 0, 1, 0]]
 
@@ -44,46 +44,67 @@ class CustomWidgets(StackLayout):
     pass
 
 class CustomLabels(Label):
+    mouse_down_x = 0
     def __init__(self, activity_text, widget_number, am):
         Label.__init__(self)
         self.text = activity_text
         self.number = widget_number
         self.am = am
-        #self.pos = (widget_number * 100 ,0)
+        print(self.width)
+        self.pos = (0,0)
         duration_arr = [12,1,2,3,4,5,6,7,8,9,10,11]
         self.duration = (duration_arr[(widget_number - 1)% 12], duration_arr[(widget_number) % 12])
-        print(str(self) + " widget#: "+str(widget_number)+" pos: "+str(self.pos)+" size: "+str(self.size))
+        print("widget#: "+str(widget_number)+" pos: "+str(self.pos)+" size: "+str(self.size))
+
+
+    global widget_count
+    global mouse_down
+    widget_number = widget_count
+    def build(self, activity_text):
+        lbl = CustomLabels(text = activity_text)
+        self.add_widget(lbl)
+
 
     def on_touch_move(self, touch):
-        if self.collide_point(*touch.pos):
-            print("on_touch_move: " +str(self) +" to new pos: "+ str(touch.pos))
-        #    return True
+#        if self.collide_point(*touch.pos):
+        if mouse_down is not None:
+            global mouse_down_pos
+            print(mouse_down_pos)
+            print("move() touch:" +str(touch.pos[0]) +" to new pos: "+ str(touch.pos))
+            mouse_offset_from_pos = (mouse_down_pos) - self.pos[0]
+            change_in_x = touch.pos[0] - (mouse_down_pos)
+            self.pos = (touch.pos[0] + change_in_x, 0)
+            return True
 
     def on_touch_up(self, touch):
-        #MousDown = False
-        if self.collide_point(*touch.pos):
-            print("on_touch_up")
+        global mouse_down
+        mouse_down = None
 
     def on_touch_down(self, touch):
         global widget_count
-        #MouseDown = True
+        global mouse_down_pos
+        global mouse_down
+        print(mouse_down)
         if self.collide_point(*touch.pos):
+            mouse_down = self
+            print("the text of the label that SHOULD move is: "  + str(self.text))
             if not(touch.is_double_tap):
                 print("on_touch_down: " +str(self) +" to new pos: "+ str(touch.pos))
+                mouse_down_pos = (touch.pos[0])
+                print(mouse_down_pos)
                 #return True
             else:
                 print("on_touch_double_click: " + str(touch.pos) + " removing: " + str(self))
-                print(self.parent.parent.parent.parent.parent.parent)
-                activity_list_layout = self.parent.parent.parent.parent.parent.ids['activity_list_layout']
+                print(self.text)
+                activity_list_layout = self.parent.parent.parent.parent.ids['activity_list_layout']
                 widget = CustomWidgets(self.text)
                 activity_list_layout.add_widget(widget)
                 widget.ids['partB'].text = self.text
                 self.parent.remove_widget(self)
                 widget_count = widget_count - 1
-                print(widget_count)
                 return True
         else:
-            print(self)
+            print("self:" + str(self) + " pos: " + str(self.pos))
 
 class CustomGridLayout(GridLayout):
     def add_activity(self):
@@ -99,22 +120,23 @@ class CustomGridLayout(GridLayout):
         pm_layout = underlying_layout.ids['pm_layout']
         # add check to see whether am or pm (true or false)
         lbl = CustomLabels(activity_text, widget_count, True)
-        scatter = Scatter()
-        scatter.do_translation_y = False
-        scatter.do_scale = False
-        scatter.do_rotation = False
-        scatter.pos = (0,0)
-        scatter.scale = 1
-                        #do_scale: False
-                        #do_rotation: False
-        scatter.add_widget(lbl)
-        am_layout.add_widget(scatter)
+#        drag = DragLabel(lbl)
+        am_layout.add_widget(lbl)
         if(len(c_label_arr) == 0):
             c_label_arr.append(lbl)
         else:
             for c_label in c_label_arr:
                 x = c_label.duration
                 print(x)
+
+#class DragLabel(Label, DragBehavior):
+#    def __init__(self,Label):
+#       super(DragLabel, self).__init__()
+#       self.drag_rect_width = self.x
+#       self.drag_rect_height = self.y
+#       self.drag_timeout = 10000000
+#       self.drag_distance = 0
+#    pass
 
 class DayPlannerApp(App):
     def build(self):
